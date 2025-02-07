253c9d14b8f1ebcc3417bc4173285ffdbaed5a0c
parshenkoval@gmail.com <parshenkoval@gmail.com>
2025-02-07 20:22:29 +0300

    Task2_2 Task2_3 Ready

---
+++ Task2_2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Угол а задан в градусах, минутах и секундах.
+            //Найти его величину в радианах.
+            //Рекомендации по тестированию: проверить работоспособность программы для углов, больших развернутого, а также для отрицательных углов.
+
+            Console.WriteLine("Введите целое число градусов угла");
+            int graduses = int.Parse(Console.ReadLine());
+            int allSecunds = graduses * 60 * 60; 
+
+            Console.WriteLine("Введите целое число минут угла");
+            int minutes = int.Parse(Console.ReadLine());
+            allSecunds = allSecunds + minutes * 60;
+
+            Console.WriteLine("Введите целое число секунд угла");
+            int secunds = int.Parse(Console.ReadLine());
+            allSecunds = allSecunds + secunds;
+
+            double gradusResult = Math.Round((double) allSecunds / 60 / 60 , 3);
+
+            double radians = Math.Round(gradusResult *  Math.PI / 180, 3);
+
+            Console.WriteLine(radians);
+        }
+    }
+}
---
+++ Task2_3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вводится целое число дюймов
+
+            int inches = int.Parse(Console.ReadLine());
+            double startValue = Math.Round(inches * 25.4, 1, MidpointRounding.ToEven);
+
+            int meters = (int)Math.Floor(startValue / 1000);
+
+            startValue = startValue - meters * 1000;
+            int centimeters = (int)Math.Floor(startValue / 10);
+
+            startValue = startValue - centimeters * 10;
+            double milimeters = Math.Round(startValue, 1);
+
+            Console.WriteLine($"{inches} дюйм = {meters} м {centimeters} см {milimeters} мм");
+        }
+    }
+}
