7835a127217661c49472d9d2e50c2e3364dc3dcd
parshenkoval@gmail.com <parshenkoval@gmail.com>
2025-02-07 18:56:42 +0300

    Task2_1 Ready

---
+++ Task2_1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вводится строка "a=1, b=2, c=3"
+
+            var ints = Console.ReadLine()?
+                .ToList()
+                .Where(x => char.IsDigit(x))
+                .Select(x => Int32.Parse(x.ToString()))
+                .ToList();
+
+            var result = new List<int>() { ints[ints.Count - 1] };
+            ints.RemoveAt(ints.Count - 1);
+            foreach (var item in ints) {result.Add(item); }
+
+            Console.WriteLine(string.Join(",", result));
+            Console.ReadLine();
+        }
+    }
+}
