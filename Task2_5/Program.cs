1ecd9705972345b69788f5878de7db51a4d0db22
parshenkoval@gmail.com <parshenkoval@gmail.com>
2025-02-16 16:53:45 +0300

    Добавлено решение 2_5

--- Task2_5/Program.cs
+++ Task2_5/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Вводится длина трубы в метрах. Округлить длину до целых в большую сторону.
+            Console.WriteLine((int)Math.Round(double.Parse(Console.ReadLine()), 0));
+            Console.ReadLine();
         }
     }
 }
