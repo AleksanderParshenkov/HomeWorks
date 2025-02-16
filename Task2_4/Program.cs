386d78b1cced963ccf52e7027475308ab2466b6d
parshenkoval@gmail.com <parshenkoval@gmail.com>
2025-02-16 16:48:43 +0300

    Добавлено решение 2_4

--- Task2_4/Program.cs
+++ Task2_4/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Вводится четырехзначное число. Поменять местами 2-ую и 4-ую цифру.
+
+            char [] charArray = Console.ReadLine().ToArray();
+            char x = charArray[1];
+            charArray[1] = charArray[2];
+            charArray[2] = x;
+
+            Console.WriteLine(string.Join("", charArray));
+            Console.ReadLine();
+
+            // Нет смыста менять строки и чары на инты при таком конкретном условии. Вводится строка - выводится строка. 
         }
     }
 }
