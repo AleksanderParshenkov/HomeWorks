0ceeef74c9fb9acf68127b1f8b224a99931be087
parshenkoval@gmail.com <parshenkoval@gmail.com>
2025-03-07 20:10:33 +0300

    fix

--- Task4_8/Program.cs
+++ Task4_8/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Вводится число. Вывести его в зеркальном виде. Количество цифр в числе заранее неизвестно. Не использовать строки
+            int x = int.Parse(Console.ReadLine());
+            int result = 0;
+            while (x > 0)
+            {
+                result = result * 10 + x % 10;
+                x = (int) Math.Round((double)( x / 10));
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
