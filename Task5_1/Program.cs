203f94a94d5f582b7b26b0a4ea8cdfb5b48e2425
parshenkoval@gmail.com <parshenkoval@gmail.com>
2025-03-20 14:44:00 +0300

    first

--- Task5_1/Program.cs
+++ Task5_1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!"); // fix
+            Console.WriteLine("Hello, World!"); // fix//
         }
     }
 }
