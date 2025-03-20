e2ef4b077f276ce23f199ea22aa66f287f513230
parshenkoval@gmail.com <parshenkoval@gmail.com>
2025-03-20 12:50:15 +0300

    fix

--- Task3_7/Program.cs
+++ Task3_7/Program.cs
@@ -7,7 +7,7 @@
             //Вводится число. Вывести «Да», если оно двухзначное, и «Нет» в противном случае.
             int x = int.Parse(Console.ReadLine());
 
-            if (Math.Abs(x /10) >= 1) Console.WriteLine("Да");
+            if (Math.Abs(x /10) >= 1 && Math.Abs(x / 10) < 10) Console.WriteLine("Да");
             else Console.WriteLine("Нет");
 
             Console.ReadLine();
