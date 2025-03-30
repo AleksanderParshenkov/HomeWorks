e310a1b6c73f2e2bf8d9177d9e63e3879cb7fe12
parshenkoval@gmail.com <parshenkoval@gmail.com>
2025-03-30 19:44:28 +0300

    Task10 Ready

---
+++ Task10_1/Building.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_1
+{
+    public class Building
+    {
+        // Поля
+        internal string adress;
+        internal int length;
+        internal int width;
+        internal int height;
+
+        // Свойства
+        public string Adress {  get { return adress; } set { adress = value; } }
+        public int Length { get { return length; } set { length = value; } }
+        public int Width { get { return width; } set { width = value; } }
+        public int Height { get { return height; } set { height = value; } }
+
+        public Building(string adress, int length, int width, int height)
+        {
+            Adress = adress;
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public void Print ()
+        {
+            Console.WriteLine($"Адрес: {Adress}\n" +
+                $"Длина: {Length}\n" +
+                $"Ширина: {Width}\n" +
+                $"Высота: {Height}");
+        }
+    }
+
+    public sealed class MultiBuilding : Building
+    {
+        internal int floors;
+        public int Floors { get { return floors; } set { floors = value; } }
+
+        public MultiBuilding(string adress, int length, int width, int height, int floors) : base(adress, length, width, height)
+        {
+            Adress = adress;
+            Length = length;
+            Width = width;
+            Height = height;
+            Floors = floors;
+        }
+
+        public void Print()
+        {
+            base.Print();            
+            Console.WriteLine($"Этажность: {Floors}");
+        }
+    } 
+}
--- Task10_1/Program.cs
+++ Task10_1/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Задан класс Building, который описывает здание. Класс содержит следующие элементы:
+            //  адрес здания;
+            //  длина здания;
+            //  ширина здания;
+            //  высота здания.
+
+            // В классе Building нужно реализовать следующие методы:
+            //  конструктор с 4 параметрами;
+            //  свойства get/ set для доступа к полям класса;
+            //  метод Print(), который выводит информацию о здании.
+
+            // Разработать класс MultiBuilding, который наследует возможности класса Building и добавляет поле этажность.
+
+            // В классе MultiBuilding реализовать следующие элементы:
+            //  конструктор с 5 параметрами – реализует вызов конструктора базового класса;
+            //  свойство get/ set доступа к внутреннему полю класса;
+            //  метод Print(), который обращается к методу Print() базового класса Building для вывода информации о всех полях класса.
+
+            // Класс MultiBuilding сделать таким, что не может быть унаследован.
+
+            // Точка входа тут, а сами классы в классе Building
+            Building building = new Building("Voronezh", 15, 25, 35);
+            building.Print();
+
+            Console.WriteLine();
+            Console.WriteLine("***");
+            Console.WriteLine();
+
+            MultiBuilding multiBuilding = new MultiBuilding("Sinye Lipyagi", 15, 25, 35, 10);
+            multiBuilding.Print();
+
+            Console.ReadKey();
         }
     }
 }
