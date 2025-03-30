4caea51f3335c8de32c3859796a7cc47de6c3e77
parshenkoval@gmail.com <parshenkoval@gmail.com>
2025-03-30 20:16:36 +0300

    fix

---
+++ Task11_1/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11_1
+{
+    internal class BankAccount <T>
+    {
+        private T id;
+
+        private string name;
+        private string surname;
+        private string patronymic;
+
+        private decimal value;
+
+
+
+    }
+}
